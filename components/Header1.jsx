de750c5e680a411713fbd73c9b657e6877361ce2
ManojkumarMuruganandham <manok10.m1@gmail.com>
2024-05-27 12:22:31 +0530

    logout

--- components/Header1.jsx
+++ components/Header1.jsx
@@ -1,41 +1,76 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import RingIcon from './Icons/RingIcon'
-import Heading from './Typography/Heading'
 
-const Header1 = ({ title, pageHeaderStyle, goBack}) => {
+
+import React from 'react';
+import { StyleSheet, View, Button, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import Heading from './Typography/Heading';
+import EvilIcon from '../components/Icons/EvilIcon';
+const Header1 = ({ title, pageHeaderStyle, goBack }) => {
+    const navigation = useNavigation();
+
+    const handleLogout = async () => {
+        try {
+            await AsyncStorage.removeItem('elarnivUsersToken');
+            console.log('Token removed from local storage');
+            navigation.navigate('Login');
+        } catch (error) {
+            console.error('Error removing token from storage:', error);
+        }
+    };
+
+    const confirmLogout = () => {
+        Alert.alert(
+            "",
+            "Are you sure you want to logout?",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Logout cancelled"),
+                    style: "cancel"
+                },
+                {
+                    text: "Logout",
+                    onPress: handleLogout,
+                    style: "destructive"
+                }
+            ],
+            { cancelable: true }
+        );
+    };
+
     return (
-    <View style={[styles.pageHeader, pageHeaderStyle]}> 
-        <View style={styles.pageTitleWrapper}>
-           <Heading title={title} />
+        <View style={[styles.pageHeader, pageHeaderStyle]}>
+            <View style={styles.pageTitleWrapper}>
+                <Heading title={title} />
+            </View>
+            <View style={styles.logoutButtonWrapper}>
+                <Button title="Logout" color="orange" onPress={confirmLogout} />
+            </View>
         </View>
-    </View>
-    )
-}
+    );
+};
 
 Header1.defaultProps = {
-    goBack:()=>{}
-}
+    goBack: () => {},
+};
 
-export default Header1
+export default Header1;
 
 const styles = StyleSheet.create({
     pageHeader: {
-        marginTop:60,
-        flexDirection:'row',
-        alignItems:'center',
-        //  marginLeft:16
+        marginTop: 60,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around'
     },
-
-    pageHeaderLeft: {
-        justifyContent:'flex-end',
-        marginLeft:16
-    },
-
     pageTitleWrapper: {
-        alignItems:'center', 
-        justifyContent:'center', 
-        textAlign:'center', 
-        width: 203 
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        width: 203
     },
-})
+    logoutButtonWrapper: {
+        justifyContent: 'flex-end',
+    },
+});
